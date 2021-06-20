91ae99fff26b4d5ef2b3a4fe018303e6af4f88ef
faiz-aiman <66171068+faiz-aiman@users.noreply.github.com>
2021-06-20 15:45:59 +0800

    changes for prob4
    - discarded all unnecessary sentence for both prob4.py and plot_music_sync.py
    
    - added cost matrix and some words for output in prob4.py

--- Problem4/plot_music_sync.py
+++ Problem4/plot_music_sync.py
@@ -1,25 +1,4 @@
 # coding: utf-8
-"""
-===============================================
-Music Synchronization with Dynamic Time Warping
-===============================================
-
-In this short tutorial, we demonstrate the use of dynamic time warping (DTW) for music synchronization
-which is implemented in `librosa`.
-
-We assume that you are familiar with the algorithm and focus on the application. Further information about
-the algorithm can be found in the literature, e. g. [1].
-
-Our example consists of two recordings of the first bars of the famous
-brass section lick in Stevie Wonder's rendition of "Sir Duke".
-Due to differences in tempo, the first recording lasts for ca. 7 seconds and the second recording for ca. 5 seconds.
-Our objective is now to find an alignment between these two recordings by using DTW.
-
-"""
-
-# Code source: Stefan Balke
-# License: ISC
-# sphinx_gallery_thumbnail_number = 4
 
 import numpy as np
 import matplotlib
@@ -29,12 +8,8 @@ import librosa
 import librosa.display
 
 
-############################################################
-# ---------------------
-# Load Audio Recordings
-# ---------------------
-# First, let's load a first version of our audio recordings.
-x_1, fs = librosa.load('Problem4/J&T Perak Mohon Maaf Secara Terbuka.wav')
+# load a first version of an audio recordings.
+x_1, fs = librosa.load('Problem4/JandT.wav')
 # And a second version, slightly faster.
 x_2, fs = librosa.load('Problem4/VoiceOver_J and T.wav')
 
@@ -46,10 +21,7 @@ ax[0].label_outer()
 librosa.display.waveplot(x_2, sr=fs, ax=ax[1])
 ax[1].set(title='Faster Version $X_2$')
 
-#########################
-# -----------------------
-# Extract Chroma Features
-# -----------------------
+# extract chroma
 hop_length = 1024
 
 x_1_chroma = librosa.feature.chroma_cqt(y=x_1, sr=fs,
@@ -68,11 +40,8 @@ librosa.display.specshow(x_2_chroma, x_axis='time',
 ax[1].set(title='Chroma Representation of $X_2$')
 fig.colorbar(img, ax=ax)
 
-
-########################
-# ----------------------
 # Align Chroma Sequences
-# ----------------------
+
 D, wp = librosa.sequence.dtw(X=x_1_chroma, Y=x_2_chroma, metric='cosine')
 wp_s = librosa.frames_to_time(wp, sr=fs, hop_length=hop_length)
 
@@ -84,14 +53,7 @@ ax.set(title='Warping Path on Acc. Cost Matrix $D$',
        xlabel='Time $(X_2)$', ylabel='Time $(X_1)$')
 fig.colorbar(img, ax=ax)
 
-##############################################
-# --------------------------------------------
-# Alternative Visualization in the Time Domain
-# --------------------------------------------
-#
-# We can also visualize the warping path directly on our time domain signals.
-# Red lines connect corresponding time positions in the input signals.
-# (Thanks to F. Zalkow for the nice visualization.)
+# alternate visualization
 from matplotlib.patches import ConnectionPatch
 
 fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(8, 4))
@@ -116,25 +78,3 @@ for tp1, tp2 in wp_s[::len(wp_s)//n_arrows]:
                           color='r', linestyle='--',
                           alpha=0.5)
     ax2.add_artist(con)
-
-###########################################################
-# -------------
-# Next steps...
-# -------------
-#
-# Alright, you might ask where to go from here.
-# Once we have the warping path between our two signals,
-# we could realize different applications.
-# One example is a player which enables you to navigate between
-# different recordings of the same piece of music,
-# e.g. one of Wagner's symphonies played by an orchestra or in a piano-reduced version.
-#
-# Another example is that you could apply time scale modification algorithms,
-# e.g. speed up the slower signal to the tempo of the faster one.
-#
-# ----------
-# Literature
-# ----------
-#
-# [1] Meinard Müller, Fundamentals of Music Processing — Audio, Analysis, Algorithms, Applications.
-# Springer Verlag, 2015.
--- Problem4/prob4.py
+++ Problem4/prob4.py
@@ -6,48 +6,7 @@ from scipy.spatial.distance import euclidean
 import librosa
 from scipy.io.wavfile import WavFileWarning, read
 
-
-
-# def dtw(s, t):
-#     y, x = len(s), len(t)
-#     dtw_matrix = np.zeros((y+1, x+1))
-#     # np.zerosReturn a new array of given shape and type, filled with zeros.
-
-#     for yindex in range(y+1):
-#         for xindex in range(x+1):
-#             dtw_matrix[yindex, xindex] = np.inf
-#     dtw_matrix[0, 0] = 0
-
-#     for yindex in range(1, y+1):
-#         for xindex in range(1, x+1):
-#             cost = abs(s[yindex-1] - t[xindex-1])
-
-#             last_min = np.min(
-#                 [dtw_matrix[yindex-1, xindex],
-#                  dtw_matrix[yindex, xindex-1],
-#                  dtw_matrix[yindex-1, xindex-1]])
-
-#             dtw_matrix[yindex, xindex] = cost + last_min
-
-#     dtw_matrix = np.delete(dtw_matrix, 0, 0)
-#     dtw_matrix = np.delete(dtw_matrix, 0, 1)
-#     return dtw_matrix
-
-
-# a = [1, 2, 3, 3, 5]
-# b = [1, 2, 3, 3, 5, 2, 2, 4, 6, 8]
-# print(a)
-# print(b)
-# print(dtw(a,b))
-
-# # using fastdtw library
-# distance, path = fastdtw(a, b, dist=euclidean)
-# print(distance)
-# print(path)
-
-# --------------------------------------------------------
-
-
+# declare variables
 VOJandT = 'Problem4/VoiceOver_J and T.wav'
 VOmemohon = 'Problem4/VoiceOver_memohon.wav'
 VOmaaf = 'Problem4/VoiceOver_maaf.wav'
@@ -55,10 +14,7 @@ JandTaudio = 'Problem4/JandT.wav'
 memohonaudio = 'Problem4/memohon.wav'
 maafaudio = 'Problem4/maaf.wav'
 
-# JandTaudioRATE, JandTaudioDATA = read(JandTaudio)
-# VOJandTRATE, VOJandTDATA= read(VOJandT)
-# memohonRATE, memohonaudioDATA = read(memohonaudio)
-
+# use librosa library to have both RATE and DATA for each audio
 VOJandTDATA, VOJandTRATE = librosa.load(VOJandT)
 VOmemohonDATA, VOmemohonRATE = librosa.load(VOmemohon)
 VOmaafDATA, VOmaafRATE = librosa.load(VOmaaf)
@@ -66,23 +22,24 @@ JandTaudioDATA, JandTaudioRATE = librosa.load(JandTaudio)
 memohonaudioDATA, memohonRATE = librosa.load(memohonaudio)
 maafaudioDATA, maafRATE = librosa.load(maafaudio)
 
-print("Rate of test audio'J&T':" + str(JandTaudioRATE) +
-      ", Original: " + str(VOJandTRATE))
-
-# data1 = np.amax(VOJandTDATA, axis=1)
-# data2 = np.amax(JandTaudioDATA, axis=1)
-# data3 = np.amax(memohonaudioDATA, axis=1)
+# print("Rate of test audio'J&T':" + str(JandTaudioRATE) +
+#       ", Original: " + str(VOJandTRATE))
+# print ((maafRATE))
 
 # print(fastdtw(data1, data2)[0])
 # print(fastdtw(data1, data3)[0])
-print(fastdtw(VOJandTDATA, JandTaudioDATA)[0])
-print(fastdtw(VOJandTDATA, memohonaudioDATA)[0])
-print(fastdtw(VOJandTDATA, maafaudioDATA)[0])
+
+print("Shortest path for VoiceOver J&T: ")
+print("with J&T: " + str(fastdtw(VOJandTDATA, JandTaudioDATA)[0]) + " and the Cost matrix is: " + str(len(VOJandTDATA)*len(JandTaudioDATA)))
+print("with memohon: " + str(fastdtw(VOJandTDATA, memohonaudioDATA)[0]) + " and the Cost matrix is: " + str(len(VOJandTDATA)*len(memohonaudioDATA)))
+print("with maaf: " + str(fastdtw(VOJandTDATA, maafaudioDATA)[0])  + " and the Cost matrix is: " + str(len(VOJandTDATA)*len(maafaudioDATA)))
 print()
-print(fastdtw(VOmemohonDATA, JandTaudioDATA)[0])
-print(fastdtw(VOmemohonDATA, memohonaudioDATA)[0])
-print(fastdtw(VOmemohonDATA, maafaudioDATA)[0])
+print("Shortest path for VoiceOver memohon: ")
+print("with J&T: " + str(fastdtw(VOmemohonDATA, JandTaudioDATA)[0]))
+print("with memohon: " + str(fastdtw(VOmemohonDATA, memohonaudioDATA)[0]))
+print("with maaf: " + str(fastdtw(VOmemohonDATA, maafaudioDATA)[0]))
 print()
-print(fastdtw(VOmaafDATA, JandTaudioDATA)[0])
-print(fastdtw(VOmaafDATA, memohonaudioDATA)[0])
-print(fastdtw(VOmaafDATA, maafaudioDATA)[0])
+print("Shortest path for VoiceOver maaf: ")
+print("with J&T: " + str(fastdtw(VOmaafDATA, JandTaudioDATA)[0]))
+print("with memohon: " + str(fastdtw(VOmaafDATA, memohonaudioDATA)[0]))
+print("with maaf: " + str(fastdtw(VOmaafDATA, maafaudioDATA)[0]))
